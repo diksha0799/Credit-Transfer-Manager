60aa5d5e0c755102058e99de4456ec3ac14b6169
diksha <65006493+diksha0799@users.noreply.github.com>
2020-09-19 13:54:04 +0530

    Update settings.py

--- creditTransferManager/settings.py
+++ creditTransferManager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*axl8v&8826-e7mr4l7ef4akpv9qn3ssrw2g!-o7-c^l^w@zo4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['credit-transfer-manager.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
