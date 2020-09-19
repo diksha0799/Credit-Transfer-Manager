601fa4b26841088ae88e68a496cd070993989e4f
diksha <65006493+diksha0799@users.noreply.github.com>
2020-09-19 13:50:59 +0530

    Update settings.py

--- creditTransferManager/settings.py
+++ creditTransferManager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*axl8v&8826-e7mr4l7ef4akpv9qn3ssrw2g!-o7-c^l^w@zo4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
